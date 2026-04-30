b3c120d5742e1bd3109cc0b298afe8d8bfed3386
nisarg <hammerbyte.nisarg@gmail.com>
2026-04-30 21:54:07 +0530

    auth fetcher

--- middlewares/parse_external_user.js
+++ middlewares/parse_external_user.js
@@ -1,6 +1,9 @@
 import { getUserByEmail } from "../db/users";
 
 module.exports = async (req, res, next) => {
-    req.user = await getUserByEmail({ email: req.headers?.email });
+    if (!req?.user) {
+        req.user = await getUserByEmail({ email: req.headers?.email });
+    }
+
     next();
 };

df85ef60360a02b48ced7d6ba2f60a2fd091d4b9
nisarg <hammerbyte.nisarg@gmail.com>
2026-05-02 07:51:18 +0530

    print body

--- routes/payment_gateway_payload_results.js
+++ routes/payment_gateway_payload_results.js
@@ -8,10 +8,15 @@ const router = libExpress.Router();
 router.post("/", async (req, res) => {
     const requiredBodyFields = ["txnid"];
 
+
+    console.log(req.body);
+
     const { isRequestBodyValid, missingRequestBodyFields, validatedRequestBody } = validateRequestBody(req.body, requiredBodyFields);
 
     if (isRequestBodyValid) {
         const { paymentGateWay: { redirectionHost, postPaymentRoute } = {} } = await readConfig("app");
+
+
 
         return res.redirect(redirectionHost.concat(postPaymentRoute.concat(validatedRequestBody.txnid)));
     }

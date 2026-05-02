e96177eccb2ddf5f4406bc25d6afae915be9632d
nisarg <hammerbyte.nisarg@gmail.com>
2026-05-02 07:56:15 +0530

    dev

--- routes/payment_gateway_payload_results.js
+++ routes/payment_gateway_payload_results.js
@@ -2,11 +2,12 @@ const libExpress = require("express");
 const { validateRequestBody } = require("sahas_utils");
 const { logger } = require("sahas_utils");
 const { readConfig } = require("../libs/config");
+const { getUserByEmail } = require("../db/users");
 
 const router = libExpress.Router();
 
 router.post("/", async (req, res) => {
-    const requiredBodyFields = ["txnid"];
+    const requiredBodyFields = ["txnid","productinfo"];
 
 
     console.log(req.body);
@@ -16,8 +17,11 @@ router.post("/", async (req, res) => {
     if (isRequestBodyValid) {
         const { paymentGateWay: { redirectionHost, postPaymentRoute } = {} } = await readConfig("app");
 
-
-
+        if(validatedRequestBody.productinfo === "Stream Selection Test") {
+            const user = await getUserByEmail({ email: req.body.email });
+            await updateUserStreamSelectionTestAllowedById({ user_id: user.id, allowed: true });
+            return res.redirect(redirectionHost.concat("stream-selection-test/attempt"));
+        }
         return res.redirect(redirectionHost.concat(postPaymentRoute.concat(validatedRequestBody.txnid)));
     }
     res.status(400).json({ error: `Missing ${missingRequestBodyFields?.join(",")}` });
